2acf79d98f721bef69daac202159c501d5d1b14b
Gabriel Henton <59454894+gsauce7@users.noreply.github.com>
2023-02-21 15:34:13 -0800

    front end started with ability to Add Clients and Remove them from the local dashboard

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const colors = require('colors');
+const cors = require('cors');
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT_BE || 5000;
 
 const app = express();
 
 // Connect to database
 connectDB();
+
+app.use(cors());
  
 app.use('/graphql', graphqlHTTP({
     schema,
